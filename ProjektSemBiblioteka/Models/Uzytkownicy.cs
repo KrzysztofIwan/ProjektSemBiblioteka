193634bc840fa7a2a8006c8b60d80d915c98f946
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2022-06-29 21:36:29 +0200

    Uzupełnienie Uzytkownicy.cs

--- ProjektSemBiblioteka/Models/Uzytkownicy.cs
+++ ProjektSemBiblioteka/Models/Uzytkownicy.cs
@@ -11,7 +11,9 @@ namespace ProjektSemBiblioteka.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
     public partial class Uzytkownicy
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,16 +22,55 @@ namespace ProjektSemBiblioteka.Models
             this.Wypozyczona_literatura = new HashSet<Wypozyczona_literatura>();
             this.Zwrot_literatury = new HashSet<Zwrot_literatury>();
         }
+
+        private int id;
+        private string imie;
+        private string nazwisko;
+        private string adres;
+        private string nrTelefonu;
+
     
-        public int Id { get; set; }
-        public string Imie { get; set; }
-        public string Nazwisko { get; set; }
-        public string Adres { get; set; }
-        public string Nr_Telefonu { get; set; }
+        public int Id { get => id; set
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        public string Imie { get => imie; set
+            {
+                imie = value;
+                OnPropertyChanged(nameof(Imie));
+            }
+        }
+        public string Nazwisko { get => nazwisko; set
+            {
+                nazwisko = value;
+                OnPropertyChanged(nameof(Nazwisko));
+            }
+        }
+        public string Adres { get => adres; set
+            {
+                adres = value;
+                OnPropertyChanged(nameof(Adres));
+            }
+        }
+        public string Nr_Telefonu { get => nrTelefonu; set
+            {
+                nrTelefonu = value;
+                OnPropertyChanged(nameof(Nr_Telefonu));
+            }
+        }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Wypozyczona_literatura> Wypozyczona_literatura { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Zwrot_literatury> Zwrot_literatury { get; set; }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
