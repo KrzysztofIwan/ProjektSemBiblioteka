b3d322e343fff1faa01a8802ff3ebbbe4ca94d7c
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2022-06-30 21:30:01 +0200

    Dodanie potrzebnych klas oraz okienek WPF

---
+++ ProjektSemBiblioteka/ViewModels/WydawnictwoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSemBiblioteka.ViewModels
+{
+    internal class WydawnictwoViewModel
+    {
+    }
+}
