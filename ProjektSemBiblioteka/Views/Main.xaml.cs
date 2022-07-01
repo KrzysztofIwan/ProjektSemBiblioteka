856df7629b901ae8ffaa7b49c0339b046178adaa
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2022-07-01 09:42:20 +0200

    Dodanie przyciskow do Main.xaml

--- ProjektSemBiblioteka/Views/Main.xaml.cs
+++ ProjektSemBiblioteka/Views/Main.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjektSemBiblioteka.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,11 @@ namespace ProjektSemBiblioteka.Views
         public Main()
         {
             InitializeComponent();
+        }
 
+        public void Show(object sender, RoutedEventArgs e)
+        {
+            DataContext = new AutorViewModel();
         }
     }
 }
